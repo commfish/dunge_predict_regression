e24fb04d131999a987091b14bd4139272154534e
Katie Palof <katie.palof@alaska.gov>
2018-04-09 16:04:14 -0800

    code for exploration of changes to the regression model. initial

---
+++ code/regression_explore.R
@@ -0,0 +1,20 @@
+# Southeast Alaska Dungeness predictive regression
+# Exploration of the currently used model with new variables
+
+# K.Palof   ADF&G
+# katie.palof@alaska.gov
+# 2018-4-09
+
+# Data is updated twice annually 1) during the first 7 days 
+#   of the season (June 15th - June 21st) and 2) after the season is complete (fall)
+
+# All catch values are in pounds unless otherwise noted.
+
+# Current regression uses the following to predict remaining season catch (total season catch - first 7days): 
+#     1. Catch in the first 7 days of the current season
+#     2. Number of permits fished in the first 7 days of the current season
+#     3. The percentage - in the previous year (year-1) - of the first 7 days catch to the total season catch
+#
+
+# Should season length be included?
+
